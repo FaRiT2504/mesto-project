f2a22f723a900fbff31627921a1ae7a8d4b84358
Farit <Faritplus@yandex.ru>
2023-03-04 15:44:13 +0500

    04.03.2023

--- index.js
+++ index.js
@@ -1,44 +1,28 @@
 ////// 1. Работа модальных окон
-////Открытие и закрытие модальных окон
+// ////Открытие и закрытие модальных окон
 // Нахожу кнопку редактировать в DOM
-const profileEdit = document.querySelector('.profile__button_type_edit');
+const profileButton = document.querySelector('.profile__button_type_edit');
 // Нахожу модальное окно "редактировать" в DOM
-const popupEdit = document.querySelector('#popup-edit');
+const popupProfile = document.querySelector('#popup-edit');
 // Нахожу кнопку добавить новое место в DOM
-const profileAdd = document.querySelector('.profile__button_type_add');
+const cardButton = document.querySelector('.profile__button_type_add');
 // Нахожу модальное окно "новое место" в DOM
-const popupAdd = document.querySelector('#popup-add');
+const popupCard = document.querySelector('#popup-add');
 //нахожу модальное окно с всплывающей картинкой в DOM
-const popupFigure = document.querySelector('#popup-figure');
-//вешаем событие на кнопку редактировать(открытие popup)
-profileEdit.addEventListener('click', function () {
-  popupEdit.classList.add('popup_opened');
-});
-//вешаем событие на кнопку добавить новое место(открытие popup)
-profileAdd.addEventListener('click', function () {
-  popupAdd.classList.add('popup_opened');
-});
-
-
-////Закрытие модальных окон
-// Нахожу кнопку закрытия для модального окна "редакторовать" в DOM
-const popupEditClose = popupEdit.querySelector('.popup__close-icon');
-// Нахожу кнопку закрытия для модального окна "новое место" в DOM
-const popupAddClose = popupAdd.querySelector('.popup__close-icon');
-// Нахожу кнопку закрытия для модального окна с всплывающей картинкой в DOM
-const popupFigureClose = popupFigure.querySelector('.popup__close-icon');
-//вешаем событие на крестик (модальное окно - редактировать)
-popupEditClose.addEventListener('click', function () {
-  popupEdit.classList.remove('popup_opened');
-});
-//вешаем событие на крестик (модальное окно - добавить)
-popupAddClose.addEventListener('click', function () {
-  popupAdd.classList.remove('popup_opened');
-});
-//вешаем событие на крестик (модальное окно с картинкой)
-popupFigureClose.addEventListener('click', function () {
-  popupFigure.classList.remove('popup_opened');
-});
+const popupPicture = document.querySelector('#popup-picture');
+//Нахожу элемент с названием картинки  в DOM
+const popupCardTitle = popupCard.querySelector('.popup__input_type_title');
+//Нахожу элемент с ссылкой на картинку  в DOM
+const popupCardLink = popupCard.querySelector('.popup__input_type_link');
+// находим формы в проекте
+const profileForm = document.forms['profile-form'];
+const cardForm = document.forms['card-form'];
+// // Нахожу кнопку закрытия для модального окна "редакторовать" в DOM
+// const profileCloseButton = popupProfile.querySelector('.popup__close');
+// // Нахожу кнопку закрытия для модального окна "новое место" в DOM
+// const cardCloseButton = popupCard.querySelector('.popup__close');
+// // Нахожу кнопку закрытия для модального окна с всплывающей картинкой в DOM
+// const pictureCloseButton = popupPicture.querySelector('.popup__close');
 
 ////Поля формы
 ////При открытии формы поля «Имя» и «О себе»  заполняются теми значениями, которые отображаются на странице.
@@ -47,29 +31,89 @@ const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 const popupName = document.querySelector('.popup__input_type_name');
 const popupJob = document.querySelector('.popup__input_type_job');
-//присваиваем значениям формы «Имя» и «О себе» значения которые отображаются на странице
-popupName.value = profileName.textContent;
-popupJob.value = profileJob.textContent;
 
+// //Нахожу кнопку "создать карточку" в DOM
+// const popupCardButtonSave = popupCard.querySelector('.popup__save');
+
+// //получаю содержимое тега template
+// const elementTemplate = document.querySelector('#element-template').content;
+//Нахожу контейнер для карточек в DOM
+const elements = document.querySelector('.elements');
+// //клонирую содержимое  тега template
+// const element = elementTemplate.querySelector('.element').cloneNode(true);
+// //нахожу картинку в модальном окне
+// const cardPicture = element.querySelector('.element__img');
+
+
+
+///Открытие модальных окон
+//функция открытия попапа
+function openPopup(popup) {
+  //присваиваем значениям формы «Имя» и «О себе» значения
+  //которые отображаются на странице
+  popupName.value = profileName.textContent;
+  popupJob.value = profileJob.textContent;
+  //открываю попап
+  popup.classList.add('popup_opened');
+};
+//вешаем событие на кнопку редактировать(открытие popup)
+profileButton.addEventListener('click', () => openPopup(popupProfile));
+//вешаем событие на кнопку добавить новое место(открытие popup)
+cardButton.addEventListener('click', () => openPopup(popupCard));
+// //вешаем событие на кнопку картинку(открытие popup)
+// cardPicture.addEventListener('click', () => openPopup(popupPicture));
+
+
+////Закрытие модальных окон
+//функция закрытия попапа
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+//находим все крестики в проекте по универсальному селектору
+const closeButton = document.querySelectorAll('.popup__close')
+//перебираем closeButton, находим крестик и вешаем событие
+closeButton.forEach((button) => {
+  //находим 1 раз ближайший к крестику попап
+  const popup = button.closest('.popup')
+  //устанавливаем отбработчик закрытия на крестик
+  button.addEventListener('click', () => closePopup(popup))
+});
+// //вешаем событие на крестик (модальное окно - редактировать)
+// profileCloseButton.addEventListener('click', () => closePopup(popupProfile));
+// //вешаем событие на крестик (модальное окно - добавить)
+// cardCloseButton.addEventListener('click', () => closePopup(popupCard));
+// //вешаем событие на крестик (модальное окно с картинкой)
+// pictureCloseButton.addEventListener('click', () => closePopup(popupPicture));
 
 ////Редактирование имени и информации о себе
-// Нахожу поле формы "редактировать" в DOM
-const formElementEdit = popupEdit.querySelector('.popup__form')
-// Нахожу поле формы "новое место" в DOM
-const formElementAdd = popupAdd.querySelector('.popup__form')
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler(evt) {
+
+//функция обработчик «отправки» формы для профиля
+function handlerProfileFormSubmit(evt) {
   // Эта строчка отменяет стандартную отправку формы.
   evt.preventDefault();
   //отображаем на странице то что сохранилось в форме «Имя» и «О себе»
   profileName.textContent = popupName.value;
   profileJob.textContent = popupJob.value;
-}
-// Прикрепляем обработчик к форме "редактировать":
-// он будет следить за событием “submit” - «отправка»
-formElementEdit.addEventListener('submit', formSubmitHandler);
+  //закрываем попап
+  closePopup(popupProfile);
 
+}
+//функция обработчик «отправки» формы для карточек
+function handlerCardFormSubmit(evt) {
+  // Эта строчка отменяет стандартную отправку формы.
+  evt.preventDefault();
+  //вызываю функцию добавления карточек
+  addElement(popupCardTitle.value, popupCardLink.value);
+  //ощищаю импуты
+  cardForm.reset();
+  //закрываю попап
+  closePopup(popupCard);
+}
+
+
+// Прикрепляем обработчик к формам:
+profileForm.addEventListener('submit', handlerProfileFormSubmit);
+cardForm.addEventListener('submit', handlerCardFormSubmit);
 
 //// 2. Добавляю шесть карточек используя template
 //готовый массив карточек
@@ -99,69 +143,61 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ];
-//Нахожу кнопку "создать карточку" в DOM
-const popupAddButtonSave = popupAdd.querySelector('.popup__save');
-//получаю содержимое тега template
-const elementTemplate = document.querySelector('#element-template').content;
-//Нахожу контейнер для карточек в DOM
-const elements = document.querySelector('.elements');
-//клонирую содержимое  тега template
-const element = elementTemplate.querySelector('.element').cloneNode(true);
-//вешаю событие на кнопку "создать карточку"
-popupAddButtonSave.addEventListener('click', function () {
-  //Нахожу элемент с названием картинки  в DOM
-  const popupAddTitle = popupAdd.querySelector('.popup__input_type_title');
-  //Нахожу элемент с ссылкой на картинку  в DOM
-  const popupAddLink = popupAdd.querySelector('.popup__input_type_link');
-  //вызываю функцию добавления карточек
-  addElement(popupAddTitle.value, popupAddLink.value);
-});
+
+// //вешаю событие на кнопку "создать карточку"
+// popupCardButtonSave.addEventListener('click', function () {
+
+//   //вызываю функцию добавления карточек
+//   addElement(popupCardTitle.value, popupCardLink.value);
+// });
+
 //добавляю в template шесть карточек из массива initialCards
-for (let i = 0; i < initialCards.length; i++) {
-  //наполняю template содержимым из массива initialCards
-  element.querySelector('.element__description').textContent = initialCards[i].name;
-  element.querySelector('.element__mask').style.backgroundImage = `url(${initialCards[i].link})`;
-  //добавляю карточку  на страницу
-  addElement(initialCards[i].name, initialCards[i].link)
-}
-// Прикрепляем обработчик к форме "новое место":
-// он будет следить за событием “submit” - «отправка»
-formElementAdd.addEventListener('submit', formSubmitHandler);
-//функция добавления карточек
-function addElement(titleValue, linkValue) {
-  //клонирую содержимое тега template
+// for (let i = 0; i < initialCards.length; i++) {
+//   // //наполняю template содержимым из массива initialCards
+//   // element.querySelector('.element__description').textContent = initialCards[i].name;
+//   // cardPicture.style.backgroundImage = `url(${initialCards[i].link})`;
+//   //добавляю карточку  на страницу
+//   addElement(initialCards[i].name, initialCards[i].link)
+// }
+
+
+//добавляю в template шесть карточек из массива initialCards
+initialCards.forEach((item) => addElement(item.name, item.link))
+
+//функция создания карточек
+function createCard(titleValue, linkValue) {
+  //получаю содержимое тега template
+  const elementTemplate = document.querySelector('#element-template').content;
+  // //Нахожу контейнер для карточек в DOM
+  // const elements = document.querySelector('.elements');
+  //клонирую содержимое  тега template
   const element = elementTemplate.querySelector('.element').cloneNode(true);
+  //нахожу картинку в модальном окне
+  const cardPicture = element.querySelector('.element__img');
   //добавляю название для карточки из поля "название" в popup
   element.querySelector('.element__description').textContent = String(titleValue);
   //добавляю ссылку для карточки из поля "Ссылка на картинку" в popup
-  element.querySelector('.element__mask').style.backgroundImage = `url(${String(linkValue)})`;
-  console.dir(element.querySelector('.element__mask').style);
-
+  cardPicture.style.backgroundImage = `url(${String(linkValue)})`;
   //вешаю событие на кнопку лайка
   element.querySelector('.element__icon').addEventListener('click', function (evt) {
     //добавляю класс element__icon_active если его нет и удаляю  если он есть
     evt.target.classList.toggle('element__icon_active');
   });
-  //вешаю событие на картинку
-  element.querySelector('.element__mask').addEventListener('click', function () {
-    //нахожу нужный div в DOM для вставки картики
-    const popupFigureMask = document.querySelector('.popup-figure__mask');
-    //вставляю картинку в модальное окно
-    popupFigureMask.style.backgroundImage = `url(${String(linkValue)})`;
-    //нахожу тег figcaption в DOM для вставки описания к картинке
-    const popupFigureCaption = document.querySelector('.popup-figure__caption');
-    //вставляю описания к картинке в модальное окно
-    popupFigureCaption.textContent = titleValue;
-    //открываю модальное окно с картинкой
-    popupFigure.classList.add('popup_opened');
-  });
-  //вешаю событие на корзину
+  //вешаем событие на  картинку(открытие popup)
+  cardPicture.addEventListener('click', () => openPopup(popupPicture));
+  // вешаю событие на корзину
   element.querySelector('.element__trash').addEventListener('click', function () {
     //удаляю карточку
     element.remove();
   });
+  return element
+}
+
+//функция добавления карточек
+function addElement(titleValue, linkValue) {
+  const card = createCard(titleValue, linkValue);
   //добавляю содержимое тега template  в DOM
-  elements.prepend(element);
+  elements.prepend(card);
 }
 
 
